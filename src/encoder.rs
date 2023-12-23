3c6d729d71ef186bd0a46ae2531f8184ba41bc7d
Boby <noreply@sliya.in>
2023-12-23 18:21:12 +0530

    sphagetti but complete

--- src/encoder.rs
+++ src/encoder.rs
@@ -24,7 +24,11 @@ impl<PIN> Encoder<PIN> where PIN: InputPin + Wait {
         }
     }
 
-    pub async fn get_val(&mut self) -> u8 {
+    /// better use get_val_avg(), as the current implementation of this
+    /// records last_high - which helps calculating aangular velocity
+    /// only when the method is called, so the later it is called, the lower
+    /// and possibly lowest more often than not.
+    async fn get_val(&mut self) -> u8 {
 
         let mut is_high: bool;
         let mut now: Instant;
@@ -37,20 +41,16 @@ impl<PIN> Encoder<PIN> where PIN: InputPin + Wait {
             now = Instant::now();
             difference = now - self.last_high;
             difference_ms = difference.as_millis();
+            log::debug!("loop: last high: {}, now: {}", self.last_high, now);
 
-            // log::info!("loop: last high: {}, now: {}", self.last_high, now);
             if difference_ms < 20 {
                 continue;
             }
-
             if is_high {
-
-
                 if self.last_state == false {
                     let answer = ((2000 / difference_ms) * 1) as u8;
                     unsafe { self.last_high = now; }
                     self.last_state = true;
-
                     return answer.max(1);
                 }
             } else {
@@ -69,10 +69,21 @@ impl<PIN> Encoder<PIN> where PIN: InputPin + Wait {
 
     /// check weather encoder is active or not.
     pub(crate) async fn active(&mut self) -> bool {
+        // return true;
         const TRIES: u64 = 3;
         const GAP_BETWEEN_TRIES: u64 =  500;
         const TOTAL_TEST_TIME: u64 = GAP_BETWEEN_TRIES * TRIES  * 2;
-        match select(self.check(), Timer::after_millis(TOTAL_TEST_TIME)).await {
+        match select((async move || {
+
+            const TRIES: u64 = 3;
+            const GAP_BETWEEN_TRIES: u64 =  500;
+            const TOTAL_TEST_TIME: u64 = GAP_BETWEEN_TRIES * TRIES  * 2;
+            for _ in 1..=TRIES {
+                let _ = self.get_val().await;
+                Timer::after_millis(GAP_BETWEEN_TRIES).await;
+            }
+
+        })(), Timer::after_millis(TOTAL_TEST_TIME)).await {
             First(x) => {
                 true
             },
@@ -81,16 +92,14 @@ impl<PIN> Encoder<PIN> where PIN: InputPin + Wait {
             }
         }
     }
-
-    async fn check(&mut self) {
-        const TRIES: u64 = 3;
-        const GAP_BETWEEN_TRIES: u64 =  500;
-        const TOTAL_TEST_TIME: u64 = GAP_BETWEEN_TRIES * TRIES  * 2;
-        for _ in 1..=TRIES {
-            let _ = self.get_val().await;
-            Timer::after_millis(GAP_BETWEEN_TRIES).await;
-        }
-    }
-
+    
+    
+    pub async fn get_voltage_level(&mut self) -> bool {
+       if self.pin.is_high().unwrap() {
+           true
+       } else {
+           false
+       }
+    } 
 
 }
--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,23 @@ mod encoder;
 
 use arrayvec::{ArrayString, ArrayVec};
 use embassy_executor::Spawner;
-use embassy_net::tcp::TcpSocket;
-use embassy_net::{Config, Ipv4Address, Stack, StackResources};
+use embassy_net::tcp::{ConnectError, TcpSocket};
+use embassy_net::{Ipv4Address, StackResources};
 use core::fmt::Write;
+use embassy_futures::select::Either::{First, Second};
+use embassy_futures::select::select;
 
 use embassy_time::{Duration, Timer};
 use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
 use esp_backtrace as _;
 use esp_println::println;
-use esp_wifi::wifi::{WifiController, WifiDevice, WifiEvent, WifiStaDevice, WifiState};
+use esp_wifi::wifi::{WifiController, WifiDevice, WifiError, WifiEvent, WifiStaDevice, WifiState};
 use esp_wifi::{initialize, EspWifiInitFor};
 use esp32c3_hal as hal;
+use esp32c3_hal::{Cpu, Delay, Rtc};
+use esp32c3_hal::gpio::{GpioPin, PullDown, RTCPinWithResistors};
+use esp32c3_hal::rtc_cntl::{get_reset_reason, get_wakeup_cause, SocResetReason};
+use esp32c3_hal::rtc_cntl::sleep::{RtcioWakeupSource, WakeupLevel};
 use hal::clock::ClockControl;
 use hal::Rng;
 use hal::{embassy, IO, peripherals::Peripherals, prelude::*, timer::TimerGroup};
@@ -30,12 +36,21 @@ const PASSWORD: &str = "bullseye";
 #[main]
 async fn main(spawner: Spawner) -> ! {
 
+    // setup logger.
     esp_println::logger::init_logger(log::LevelFilter::Info);
-
     let peripherals = Peripherals::take();
-
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let mut delay = Delay::new(&clocks);
+
+
+
+    log::info!("chip up and running!");
+    let reason = get_reset_reason(Cpu::ProCpu).unwrap_or(SocResetReason::ChipPowerOn);
+    log::info!("reset reason: {:?}", reason);
+    let wake_reason = get_wakeup_cause();
+    log::info!("wake reason: {:?}", wake_reason);
 
 
 
@@ -44,9 +59,20 @@ async fn main(spawner: Spawner) -> ! {
     embassy::init(&clocks, timer_group0.timer0);
 
 
+    // SETUP ENCODER
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut pin = io.pins.gpio6.into_pull_down_input();
+    let mut pin2 = io.pins.gpio5.into_pull_down_input();
+    let mut encoder = Encoder::new(pin);
+    // if !encoder.active().await  {
+    //
+    //
+    // }
+    log::info!("encoder active continuing to fetch and broadcast");
+
     // SETUP WIFI
     let timer = hal::systimer::SystemTimer::new(peripherals.SYSTIMER).alarm0;
-    let init = initialize(
+    let wifi_init = initialize(
         EspWifiInitFor::Wifi,
         timer,
         Rng::new(peripherals.RNG),
@@ -54,16 +80,15 @@ async fn main(spawner: Spawner) -> ! {
         &clocks,
     ).unwrap();
 
-    let wifi = peripherals.WIFI;
     let (wifi_interface, controller) =
-        esp_wifi::wifi::new_with_mode(&init, wifi, WifiStaDevice).unwrap();
+        esp_wifi::wifi::new_with_mode(&wifi_init, peripherals.WIFI, WifiStaDevice).unwrap();
 
 
-    let config = Config::dhcpv4(Default::default());
+    let config = embassy_net::Config::dhcpv4(Default::default());
     let seed = 1234; // very random, very secure seed
 
     // Init network stack
-    let stack = &*make_static!(Stack::new(
+    let stack = &*make_static!(embassy_net::Stack::new(
         wifi_interface,
         config,
         make_static!(StackResources::<3>::new()),
@@ -71,124 +96,221 @@ async fn main(spawner: Spawner) -> ! {
     ));
 
 
-    // SETUP ENCODER
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let pin = io.pins.gpio6.into_pull_down_input();
-    let mut encoder = Encoder::new(pin);
 
-    if !encoder.active().await  {
-        log::info!("encoder inactive deep sleeping...");
-        // deep sleep
-    }
 
-    log::info!("encoder active continuing to fetch and broadcast");
+    // SPAWN EMBASSY TASKS
     spawner.spawn(connection(controller)).ok();
     spawner.spawn(net_task(&stack)).ok();
 
+
+    // FIRMWARE LOGIC
     let mut rx_buffer = [0; 4096];
     let mut tx_buffer = [0; 4096];
 
-    loop {
+    let mut go_to_sleep = false;
+
+    for i in 1..=10 {
+        log::info!("waiting for link to be up {}th time...", i);
         if stack.is_link_up() {
+            log::info!("link is up!");
             break;
         }
-        Timer::after(Duration::from_millis(500)).await;
-    }
 
-    log::info!("Waiting to get IP address...");
-    loop {
-        if let Some(config) = stack.config_v4() {
-            log::info!("Got IP: {}", config.address);
+        if i >= 10 {
+            go_to_sleep = true;
             break;
         }
-        Timer::after(Duration::from_millis(500)).await;
+
+        Timer::after(Duration::from_secs(1)).await;
     }
 
-    loop {
-        Timer::after(Duration::from_millis(1_000)).await;
-
-        let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
-
-        socket.set_timeout(Some(embassy_time::Duration::from_secs(100)));
-
-        let remote_endpoint = (Ipv4Address::new(192, 168, 29, 182), 8080);
-        println!("connecting...");
-        let r = socket.connect(remote_endpoint).await;
-        if let Err(e) = r {
-            println!("connect error: {:?}", e);
-            continue;
-        }
-        println!("connected!");
-        let mut buf = [0; 1024];
+    if !go_to_sleep {
+        log::info!("Waiting to get IP address...");
         loop {
-            use embedded_io_async::Write;
-            log::info!("getting val from sensor...");
-            let val = encoder.get_val_avg(3).await;
-
-            let mut  strr = ArrayString::<100>::new();
-            write!(&mut strr, "GET /update/{}  HTTP/1.0\r\nHost: 192.168.29.182:8080\r\n\r\n", val).expect("failed to write GET request to socket.");
-            socket.write(strr.as_bytes()).await.unwrap();
-            socket.flush().await.unwrap();
-            // let r = socket
-            //     .write_all(b"GET / HTTP/1.0\r\nHost: www.mobile-j.de\r\n\r\n")
-            //     .await;
-            if let Err(e) = r {
-                println!("write error: {:?}", e);
+            if let Some(config) = stack.config_v4() {
+                log::info!("Got IP: {}", config.address);
                 break;
             }
-            let n = match socket.read(&mut buf).await {
-                Ok(n) => {
-                    break;
-                },
-                Err(e) => {
-                    println!("read error: {:?}", e);
-                    break;
-                }
-            };
+            Timer::after(Duration::from_millis(500)).await;
         }
-        Timer::after(Duration::from_millis(300)).await;
+    }
+
+    loop {
+        let mut val: f64 = 0.0;
+        if !go_to_sleep {
+            log::info!("getting val from sensor...");
+            match select(encoder.get_val_avg(4), Timer::after_millis(5_000)).await {
+                First(x) => {
+                    val = x;
+                }
+                Second(_) => {
+                    log::info!("did not get sensor data for 5s, sleeping...");
+                    go_to_sleep = true;
+                }
+            }
+        }
+
+
+        if !go_to_sleep {
+            let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
+            socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+
+            log::info!("connecting to server...");
+            let remote_endpoint = (Ipv4Address::new(192, 168, 29, 48), 8080);
+
+            let mut continue_with_socket = false;
+            for try_count in 1..=5 {
+                let conn_result = socket.connect(remote_endpoint).await;
+                match conn_result {
+                    Err(e) => {
+                        match e {
+                            ConnectError::TimedOut | ConnectError::ConnectionReset => {
+                                log::info!("error connecting to server: {:?} on try {}. retrying  after 1 sec", e, try_count);
+                                Timer::after_secs(1).await;
+                            }
+                            _ => {
+                                log::info!("error connecting to server: {:?} on try {}. continuing to connect to socket anyway", e, try_count);
+                                continue_with_socket = true;
+                                break;
+                            }
+
+
+                        }
+                    }
+
+                    Ok(x) => {
+                        continue_with_socket = true;
+                        break;
+                    }
+                }
+
+
+                if try_count == 5 {
+                    log::info!("5 retries exhausted to connect to server.");
+                    go_to_sleep = true;
+                }
+
+
+
+            }
+
+            if continue_with_socket  {
+                log::info!("connected to server!");
+                let mut buf = [0; 1024];
+
+
+                let mut  strr = ArrayString::<100>::new();
+                write!(&mut strr, "GET /update/{}  HTTP/1.0\r\nHost: 192.168.29.182:8080\r\n\r\n", val).expect("failed to write GET request to socket.");
+
+                if let Err(e) = socket.write(strr.as_bytes()).await {
+                    log::info!("couldn't write to socket: {:?}", e);
+
+                };
+
+                if let Err(e) = socket.flush().await {
+                    log::info!("failed to flush to socket, will retry to get val and submit again.");
+                    continue
+
+                };
+
+                log::info!("reading socket response fully...");
+                loop {
+                    match socket.read(&mut buf).await {
+                        Ok(n) => {
+                            if n == 0 {
+                                break;
+                            } else {
+                                continue;
+                            }
+                        }
+
+                        Err(e) => {
+                            println!("failed to read data from socket. error: {:?}", e);
+                            break;
+                        }
+                    };
+
+                }
+            }
+        }
+
+
+
+        if go_to_sleep {
+            // go to deep sleep
+
+            let encoder_high = encoder.get_voltage_level().await;
+            log::info!("encoder state now: {}, waking up for opposite state then", encoder_high);
+
+            let wakeup_level = if encoder_high {
+                WakeupLevel::Low
+            } else {
+                WakeupLevel::High
+            };
+
+            let wakeup_pins: &mut [(&mut dyn RTCPinWithResistors, WakeupLevel)] = &mut [
+                (&mut pin2, wakeup_level),
+            ];
+
+            let rtcio_wakeup_source = RtcioWakeupSource::new(wakeup_pins);
+            log::info!(" deep sleeping...");
+            rtc.sleep_deep(&[&rtcio_wakeup_source], &mut delay);
+        }
+
+        Timer::after(Duration::from_millis(500)).await;
     }
 }
+
+
+
 
 #[embassy_executor::task]
 async fn connection(mut controller: WifiController<'static>) {
-    println!("start connection task");
-    println!("Device capabilities: {:?}", controller.get_capabilities());
+    log::info!("start connection task");
+    log::info!("Device capabilities: {:?}", controller.get_capabilities());
     loop {
-        match esp_wifi::wifi::get_wifi_state() {
-            WifiState::StaConnected => {
-                // wait until we're no longer connected
-                controller.wait_for_event(WifiEvent::StaDisconnected).await;
-                Timer::after(Duration::from_millis(5000)).await
+        if esp_wifi::wifi::get_wifi_state() == WifiState::StaConnected {
+            // wait until we're no longer connected
+            controller.wait_for_event(WifiEvent::StaDisconnected).await;
+            log::info!("no longer connected to wifi AP");
+            Timer::after(Duration::from_millis(5000)).await
+        }
+        match controller.is_started() {
+            Ok(started) => {
+                if !started {
+                    log::info!("controller not started");
+                    let client_config = Configuration::Client(ClientConfiguration {
+                        ssid: SSID.try_into().unwrap(),
+                        password: PASSWORD.try_into().unwrap(),
+                        ..Default::default()
+                    });
+                    controller.set_configuration(&client_config).unwrap();
+                    log::info!("Starting wifi");
+                    controller.start().await.unwrap();
+                    log::info!("Wifi started!");
+                }
+            },
+
+            Err(e) => {
+                log::info!("some error occured: {:?}", e);
             }
-            _ => {}
+
         }
-        if !matches!(controller.is_started(), Ok(true)) {
-            let client_config = Configuration::Client(ClientConfiguration {
-                ssid: SSID.try_into().unwrap(),
-                password: PASSWORD.try_into().unwrap(),
-                ..Default::default()
-            });
-            controller.set_configuration(&client_config).unwrap();
-            println!("Starting wifi");
-            controller.start().await.unwrap();
-            println!("Wifi started!");
-        }
-        println!("About to connect...");
+        log::info!("About to connect...");
 
         match controller.connect().await {
-            Ok(_) => println!("Wifi connected!"),
+            Ok(_) => log::info!("Wifi connected!"),
             Err(e) => {
-                println!("Failed to connect to wifi: {e:?}");
-                Timer::after(Duration::from_millis(5000)).await
+                log::info!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(1000)).await
             }
         }
     }
 }
 
 #[embassy_executor::task]
-async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
-    log::info!("doing work on stack");
+async fn net_task(stack: &'static embassy_net::Stack<WifiDevice<'static, WifiStaDevice>>) {
+    log::info!("task#net_taask doing work on embassy stack");
     stack.run().await
 }
 
